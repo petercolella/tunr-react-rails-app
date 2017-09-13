7345d4a80993929eea842c75b395ae5798487b20
petercolella <peter.joseph.colella@gmail.com>
2017-09-13 07:30:54 -0400

    added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import ArtistList from './components/ArtistList';
+import Artist from './components/Artist';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <div>
+            <h1>Tunr</h1>
+            <div>
+             <Link to="/">Artists</Link>
+             <Link to="/artist/1">Single Artist</Link>
+            </div>
+          </div>
+          <Route exact path="/" component={ArtistList} />
+          <Route path="/artist/:id" component={Artist} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
